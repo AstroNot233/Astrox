feba87784d71fce520f702d753bb6f31b2555297
Astronot233 <tyz.err.233@gmail.com>
2026-09-23 00:34:47 +0800

    INIT

---
+++ hardware-config.nix
@@ -0,0 +1,19 @@
+abort
+''*
+╔══════════════════════════════════════════════════════════════╗
+║                                                              ║
+║  ERROR       This is a placeholder!                          ║
+║  Please use                                                  ║
+║      nixos-generate-config --show-hardware-config            ║
+║  and redirect to `hardware-config.nix`.                      ║
+║                                                              ║
+║ ════════════════════════════════════════════════════════════ ║
+║                                                              ║
+║  CAUTION     Never commit the actual `hardware-config.nix`!  ║
+║                                                              ║
+║  Please execute                                              ║
+║      git update-index --skip-worktree hardware-config.nix    ║
+║  instantly in case you commit it accidentally.               ║
+║                                                              ║
+╚══════════════════════════════════════════════════════════════╝
+''
